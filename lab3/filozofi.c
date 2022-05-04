feea763d13fdc66664afed3a411626dda79594c3
Rene Javorić Ilić <70699933+Rene0108@users.noreply.github.com>
2022-05-04 13:38:52 +0200

    Update filozofi.c

--- lab3/filozofi.c
+++ lab3/filozofi.c
@@ -11,7 +11,6 @@ int vilica[5] = {1, 1, 1, 1, 1};
 pthread_cond_t red[5];
 pthread_mutex_t monitor;
 
-
 // kreiran "bespotrebni" kritični odsječak zbog bolje preglednosti koda
 ulaz_u_KO() {
     pthread_mutex_lock(&monitor);
@@ -21,7 +20,7 @@ izlaz_iz_KO() {
     pthread_mutex_unlock(&monitor);
 }
 
-void jesti(int n){
+void jesti(int n) {
 
         ulaz_u_KO(); // pthread_mutex_lock(&monitor);
 
@@ -45,7 +44,7 @@ void jesti(int n){
             filozof[n] = 'O';
             vilica[n] = vilica[(n + 1) % 5] = 1;
 
-            if(n == 0){
+            if (n == 0) {
                 pthread_cond_signal(&red[4]);
             } else {
                 pthread_cond_signal(&red[n - 1]);
@@ -55,14 +54,12 @@ void jesti(int n){
 
         izlaz_iz_KO(); // pthread_mutex_unlock(&monitor);
 }
-
-
-void misliti(){
+ 
+void misliti() {
     sleep(3);
 }
 
-
-void *filozofi(void *i){
+void *filozofi(void *i) {
     while(1) {
         misliti();
         jesti((int) i);
@@ -70,17 +67,17 @@ void *filozofi(void *i){
 }
 
 
-int main(){
+int main() {
 
     pthread_t dretve[5];
 
     pthread_cond_init(red, NULL);
     pthread_mutex_init(&monitor, NULL);
 
-    for(int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 5; ++i) {
         pthread_create(&dretve[i], NULL, (void *) filozofi, (void *) i);
     }
-    for(int i = 0; i < 5; ++i){
+    for (int i = 0; i < 5; ++i) {
         pthread_join(dretve[i], NULL);
     }
 
