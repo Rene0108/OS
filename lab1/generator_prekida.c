9a1a26aec89d0f2c4a2290b335dedc9e6a31ec6b
Rene Javorić Ilić <70699933+Rene0108@users.noreply.github.com>
2022-06-09 22:15:10 +0200

    Update generator_prekida.c

--- lab1/generator_prekida.c
+++ lab1/generator_prekida.c
@@ -6,24 +6,24 @@
 int pid = 0;
 int sig[] = {SIGFPE, SIGHUP, SIGILL, SIGSYS};
 
-void prekidna_rutina(int sig){
-   kill(pid, SIGKILL);
-   exit(0);
+void prekidna_rutina(int sig) {
+      kill(pid, SIGKILL);
+      exit(0);
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
    
-   pid=atoi(argv[1]);
-   sigset(SIGINT, prekidna_rutina);
-   srand((unsigned) time(NULL));
+      pid = atoi(argv[1]);
+      sigset(SIGINT, prekidna_rutina);
+      srand((unsigned) time(NULL));
 
-   int t;
+      int t;
 
-   while(1){
-      sleep(rand() %(5 - 3 + 1) + 3);
-      t = rand() % (3 - 0 + 1) + 0;
-      printf(" Šaljem %d \n", t);
-      kill(pid, sig[t]);
-   }
-   return 0;
+      while(1) {
+            sleep(rand() %(5 - 3 + 1) + 3);
+            t = rand() % (3 - 0 + 1) + 0;
+            printf(" Šaljem %d \n", t);
+            kill(pid, sig[t]);
+      }
+      return 0;
 }
